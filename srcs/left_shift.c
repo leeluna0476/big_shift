eade6eb1a6625c8c2dc017472751f214bb9de662
LEE LUNA <seojilee@student.42seoul.kr>
2025-11-20 22:09:47 +0900

    fix bug where the last half-byte being overwritten

--- srcs/left_shift.c
+++ srcs/left_shift.c
@@ -4,11 +4,11 @@ void left_shift(const unsigned char *big_int, unsigned char *buf, size_t size, s
 	size_t Nbytes = Nbits / 8;
 	size_t Nbits_mod = Nbits % 8;
 
-	for (size_t i = Nbytes; i < size; ++i) {
+	for (size_t i = Nbytes; i < size - 1; ++i) {
 		buf[i - Nbytes] = (big_int[i] << Nbits_mod) | (big_int[i  + 1] >> (8 - Nbits_mod));
 	}
 	buf[size - 1 - Nbytes] = big_int[size - 1] << Nbits_mod;
-	for (int i = size - Nbytes; i < size; ++i) {
+	for (size_t i = size - Nbytes; i < size; ++i) {
 		buf[i] = 0;
 	}
 }
