ac1e02d6c4272f647bd291a09c4e1667ff7175b9
LEE LUNA <seojilee@student.42seoul.kr>
2025-11-20 21:55:08 +0900

    rename the func assign to atoi_bigint

--- include/big_shift.h
+++ include/big_shift.h
@@ -20,7 +20,7 @@ typedef struct {
 void left_shift(const unsigned char *big_int, unsigned char *buf, size_t size, size_t Nbits);
 
 // returns bytes of the converted data pointed by *dst
-size_t assign(const char *src, unsigned char **dst, base_t base);
+size_t atoi_bigint(const char *src, unsigned char **dst, base_t base);
 char *itoa_bigint(const unsigned char *big_int, size_t size, base_t base);
 
 #endif
