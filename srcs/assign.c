01e24eba01f7fb03d40b3b8565bc8adc905af4fb
LEE LUNA <seojilee@student.42seoul.kr>
2025-11-20 20:54:17 +0900

    add a feature assigning binary

--- srcs/assign.c
+++ srcs/assign.c
@@ -1,14 +1,9 @@
 #include "big_shift.h"
 
-static size_t assign_hex(const char *src, unsigned char **dst) {
-	size_t slen = strlen(src);
-	if (!slen) {
-		return 0;
-	}
-
+static size_t assign_hex(const char *src, unsigned char **dst, size_t slen) {
 	size_t bits = slen * 4;
 	size_t bytes = bits % 8 ? bits / 8 + 1 : bits / 8;
-	*dst = malloc(sizeof(unsigned char) * bytes);
+	*dst = malloc(bytes);
 	if (!(*dst)) {
 		return 0;
 	}
@@ -30,6 +25,38 @@ static size_t assign_hex(const char *src, unsigned char **dst) {
 	return bytes;
 }
 
+static size_t assign_bin(const char *src, unsigned char **dst, size_t slen) {
+	size_t bytes = slen % 8 ? slen / 8 + 1 : slen / 8;
+	*dst = malloc(bytes);
+	if (!(*dst)) {
+		return 0;
+	}
+
+	size_t si = slen;
+	size_t bi = bytes;
+	while (bi > 0) {
+		unsigned char u = 0;
+		size_t end = si >= 8 ? si - 8 : 0;
+		size_t bitlen = si - end;
+		while (si > end) {
+			unsigned char shifts = bitlen - (si - end);
+			u |= (src[si - 1] - '0') << shifts;
+			--si;
+		}
+		(*dst)[bi - 1] = u;
+		--bi;
+	}
+
+	return bytes;
+}
+
 size_t assign(const char *src, unsigned char **dst, base_t base) {
-	return assign_hex(src, dst);
+	static size_t (*fp[3])(const char *, unsigned char **, size_t) = { assign_hex, assign_hex, assign_bin };
+
+	size_t slen = strlen(src);
+	if (!slen) {
+		return 0;
+	}
+
+	return fp[base](src, dst, slen);
 }
